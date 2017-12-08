65b28c7f719fcba4b886d75513ac6799bc425cf1
Rebecca Strauss <rstrauss127@gmail.com>
2017-12-08 19:33:40 +0000

    automatically backed up by learn

---
+++ lib/person.rb
@@ -0,0 +1,17 @@
+class Person
+  def name=(firstname)
+    @name = firstname
+  end
+
+  def name
+    @name
+  end
+
+  def job=(occupation)
+    @job = occupation
+  end
+
+  def job
+    @job
+  end
+end
